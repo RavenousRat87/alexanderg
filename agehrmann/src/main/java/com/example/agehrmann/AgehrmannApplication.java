d8fa4f76b568b04be2d07944a0cbda12424a0c7e
agehrmann <gehrmann.alexandergmx.net>
2020-06-19 14:03:16 +0200

    initial commit

---
+++ agehrmann/src/main/java/com/example/agehrmann/AgehrmannApplication.java
@@ -0,0 +1,13 @@
+package com.example.agehrmann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgehrmannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgehrmannApplication.class, args);
+	}
+
+}

54d7504f6b64e29d0606095b96c1b2125f40063f
agehrmann <gehrmann.alexandergmx.net>
2020-06-21 12:55:23 +0200

    changed projectname

--- agehrmann/src/main/java/com/example/agehrmann/repository/IDeliveryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.agehrmann.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.agehrmann.entities.Delivery;
-
-public interface IDeliveryRepository extends MongoRepository<Delivery, String> {
-	
-}
